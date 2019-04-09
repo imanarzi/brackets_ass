f294664184001069b03b29d765a9cfb3b76b3085
Iman Anwarzai <ianwarzai18@src-code.simons-rock.edu>
2019-04-09 19:09:51 -0400

    done

--- check_brackets.c
+++ check_brackets.c
@@ -13,20 +13,49 @@ int main() {
     printf("%d characters were read.\n",input_len);
     // printf("The input was: '%s'\n",input_buffer);
 	fflush(stdout);
-    Stack * opening_brackets_stack = createStack(max_line); 
+    Stack * opening_brackets_stack = createStack(max_line);
+	int r = -1; 
     for (int position = 0; position < input_len; ++position) {
         char next = input_buffer[position];
 
         if (next == '(' || next == '[' || next == '{') {
             // Process opening bracket, write your code here
+            	Bracket b;
+		//b = malloc(sizeof(Bracket));
+	    	if(next == '(')
+			b.type = 0;
+		if(next == '[')
+			b.type = 1;
+		if(next == '{')
+			b.type = 2;
+            	push(opening_brackets_stack, b);
         }
 
         if (next == ')' || next == ']' || next == '}') {
             // Process closing bracket, write your code here
-        }
+       		if(next == ')' && pop(opening_brackets_stack).type != 0){
+			r = position;
+			break;
+		}
+		if(next == ']' && pop(opening_brackets_stack).type != 1){
+			r = position;
+			break;
+		}
+		if(next == '}' && pop(opening_brackets_stack).type != 2){
+			r = position;
+			break;
+		}
+        	//break;
+	}
     }
 
     // Printing answer, write your code here
 	printf("My result is:\n");
+	if(r >= 0)
+                printf("%d\n", r + 1);
+	else if(!isEmpty(opening_brackets_stack))
+                printf("%d\n", pop(opening_brackets_stack).position + 1);
+	else
+		printf("Success\n"); 
     return 0;
 }
--- stack.c
+++ stack.c
@@ -7,20 +7,25 @@ Stack* createStack(unsigned capacity){
     stack->capacity = capacity; 
     stack->top = -1; 
     //TODO: Your choice here:
-	//stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
-	//StackNode* head;	
+	stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
+	//StackNode* head;
+		
     return stack; 
 }
 
 // Stack is full when top index is equal to capacity-1
 int isFull(Stack* stack){
 	//TODO
+	if(stack->capacity - 1 == stack->top)
+		return 1;
 	return 0;
 }
 
 // Stack is empty when top is equal to -1 (or head of the list is NULL)
 int isEmpty(Stack* stack){
 	//TODO
+	if(stack->top == -1)
+		return 1;
 	return 0;
 }
 
@@ -29,12 +34,16 @@ void push(Stack* stack, Bracket item){
 	if (isFull(stack)) 
         return; 
     //TODO
+	stack->array[stack->top + 1] = item;
+	stack->top++;
+	item.position = stack->top;
 }
 
 // Function to remove an item from stack and return it. 
 Bracket pop(Stack* stack){
 	//TODO
-	Bracket top;
+	Bracket top = stack->array[stack->top];
+	stack->top--;
 	return top;
 }    
   
